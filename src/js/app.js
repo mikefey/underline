c9315c4d61869254f68d1c1eb5e53d687ffcb02b
Mike Fey <mikefey@protonmail.com>
2018-06-06 23:07:10 -0400

    Add check for iOS before installing service worker, update add to homescreen icon

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,7 @@ import * as OfflinePluginRuntime from 'offline-plugin/runtime';
 import Main from './views/main/Main.jsx';
 
 (() => {
+  const iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
   const doc = document;
   const appContainer = doc.getElementsByClassName('app-container')[0];
 
@@ -13,7 +14,9 @@ import Main from './views/main/Main.jsx';
     appContainer,
   );
 
-  if (process.env.NODE_ENV === 'production') {
+  // Only install service worker in production and don't install
+  // it on iOS because it wil cause the DropBox chooser to fail.
+  if (process.env.NODE_ENV === 'production' && !iOS) {
     OfflinePluginRuntime.install();
   }
 })();
